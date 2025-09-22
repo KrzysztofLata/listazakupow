e10f070c09f0d4a173360d842f0daf3a6801ef9e
KrzysztofLata <133794261+KrzysztofLata@users.noreply.github.com>
2025-09-22 10:14:44 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<Item> Products { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Products = new ObservableCollection<Item>();
+            BindingContext = this;
+        }
+
+        private void AddBtn_Clicked(object sender, EventArgs e)
+        {
+            string itemname = additem.Text;
+            String quantity = setquantity.Text;
+            if (quantity==null || quantity=="")
+            {
+                quantity = "1";
+            }
+            if (string.IsNullOrEmpty(itemname))
+                return;
+            additem.Text = "";
+            setquantity.Text = "";
+            var existing = Products.FirstOrDefault(p => p.Name.Equals(itemname, StringComparison.OrdinalIgnoreCase));
+
+            if (existing != null)
+            {
+                existing.Quantity++;
+                Products.Remove(existing);
+                Products.Add(existing);
+            }
+            else
+            {
+                Products.Add(new Item { Name = itemname, Quantity = Int32.Parse(quantity) });
+            }
+
+            collectionview.ItemsSource = Products;
+        }
+
+        private void DelBtn_Clicked(object sender, EventArgs e)
+        {
+            var selected = collectionview.SelectedItem;
+            Products.Remove((Item)selected);
+        }
+    }
+}
